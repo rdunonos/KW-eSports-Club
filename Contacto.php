cae79e1ff92a847c7cf8ca1d1613a5569e3f3007
chuty1 <jsalinas@jaumebalmes.net>
2018-05-29 18:12:22 +0200

    CONTACTO-FORM
    Contacto.php --> carpeta  app\
    create_contactos_table--> database\migrations (hacer un php artisan migrate)para subir la tabla contactos a la BD
    contacto.blade.php--> resources\view\
    las carpteas (image-js-css-vendor-fonts)- van dentro de  la carpeta public

---
+++ Contacto.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Contacto extends Model
+{
+    protected $fillable = [
+      'name',
+      'email',
+      'comments'
+    ];
+}
